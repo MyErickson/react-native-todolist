044fc25b4c829fd707c53357d4d3d4e79fb548a8
robert erickson <ericksond.robert@gmail.com>
2019-08-29 20:46:23 +0200

    header & style

---
+++ components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text , View } from 'react-native';
+import { style } from './style'
+
+
+const Header = ( ) => {
+    return(
+        <View style={style.subHeader}>
+            <View>
+                 <Text> Hello erickson</Text>
+            </View>
+
+        </View>
+    )
+}
+
+export default Header
---
+++ components/header/style.js
@@ -0,0 +1,8 @@
+ import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+    subHeader: {
+        backgroundColor: 'blue',
+        height: 30
+    }
+})
